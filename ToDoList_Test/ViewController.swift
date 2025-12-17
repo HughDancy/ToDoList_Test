37c380bb5c8112bb04c053e67f29969fda0fee66
BorisKiselev <hugh.dancy@icloud.com>
2025-12-17 11:28:30 +0700

    Initial Commit

---
+++ ToDoList_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList_Test
+//
+//  Created by Борис Киселев on 17.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
